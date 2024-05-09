7899402a734a337571b44098b6bc9018ac078125
VinijSantos <132084888+VinijSantos@users.noreply.github.com>
2024-05-09 09:42:30 -0300

    Delete Feedpet-main (2) directory

--- Feedpet-main (2)/Feedpet-main/Feedpet-main/Project/routes/landing.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, url_for
-
-landing_route = Blueprint('landing', __name__)
-
-
-@landing_route.route('/')
-def landing_page():
-    return render_template('index.html')
--- Feedpet-main (2)/Feedpet-main/Feedpet-main/Project/static/menupage/javascript/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('open_btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
-});
